9aa8c5010f47f275840e0c7554249a7f158e293d
vijay2442 <34646551+vijay2442@users.noreply.github.com>
2017-12-20 16:14:39 +0530

    Create charswp.c

---
+++ file/charswp.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(void)
+{
+char a[10];
+int count,i,j,k;
+scanf("%s",&a);
+for(i=0;i<strlen(a);i++)
+{
+count=1;
+for(j=i+1;j<strlen(a);j++)
+{
+if(a[i]==a[j])
+{
+count++;
+for(k=j+1;k<strlen(a);k++) 
+a[k-1]=a[k];
+a[k-1]=strlen(a);
+}
+}
+printf("%c - %d\n",a[i],count);
+}
+return 0;
+}
